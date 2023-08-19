b4d9c2de352370419dfeb47e019b747c5288ce9f
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2023-08-20 02:30:04 +0800

    jenkins initiated ssm test

--- reformat_ephemeral.py
+++ reformat_ephemeral.py
@@ -8,10 +8,6 @@ ssm_json = ssm_file.read()
 instance_ids = os.environ['Instance ID'].split('\n')
 target_region = os.environ['Region']
 
-print(instance_ids)
-print(target_region)
-
-
 region_lookup = {
 	"USEA":"us-east-1",
 	"USWE":"us-west-1",
@@ -22,17 +18,21 @@ region_lookup = {
 	"APAU":"ap-southeast-2"
 }
 
-target_region = region_lookup["USEA"]
+target_region = region_lookup[target_region]
 
 ssm_doc_name = 'test-hyperthreading-ephemeral'
 
 ssm_client = boto3.client('ssm',  region_name = target_region)
 
-# ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
-# print(ssm_create_response)
+ssm_create_response = ssm_client.create_document(Content = ssm_json, Name = ssm_doc_name, DocumentType = 'Command', DocumentFormat = 'JSON', TargetType =  "/AWS::EC2::Instance")
+print(ssm_create_response)
 
-# ssm_run_response = ssm_client.send_command(InstanceIds = instance_ids, DocumentName=ssm_doc_name,DocumentVersion="$DEFAULT", TimeoutSeconds=120,)
-# print(ssm_run_response)
+for instance in instance_ids:
+	print(instance)
 
-ssm_delete_response = ssm_client.delete_document(Name=ssm_doc_name)
-print(ssm_delete_response)
+# for instance in instance_ids:
+# 	ssm_run_response = ssm_client.send_command(InstanceIds = [instance], DocumentName=ssm_doc_name, DocumentVersion="$DEFAULT", TimeoutSeconds=120)
+# 	print(ssm_run_response)
+
+# ssm_delete_response = ssm_client.delete_document(Name=ssm_doc_name)
+# print(ssm_delete_response)
