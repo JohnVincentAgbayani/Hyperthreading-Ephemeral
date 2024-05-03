13d27632253316a1219e497081970e0a0c93e3b7
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2024-05-03 17:02:45 +0800

    restructured logging process

--- reformat_ephemeral.py
+++ reformat_ephemeral.py
@@ -5,6 +5,7 @@ import os
 from datetime import datetime
 
 def main():
+
 	ssm_file = open("ssm_ephemeral.json")
 	ssm_json = ssm_file.read()
 
@@ -42,7 +43,35 @@ def main():
 	with open('log_data.json', 'w') as outfile:
 	    json.dump(data_log, outfile)
 
-	print(data_log)    
+	return data_log  
 
 
-main()
+def write_json_s3(data_log):
+	
+	goss_key_file = open("goss_key.txt")
+	goss_key = goss_key_file.read()
+	goss_key = goss_key.replace("\n","").strip()
+
+	goss_secret_file = open("goss_secret.txt")
+	goss_secret = goss_secret_file.read()
+	goss_secret = goss_secret.replace("\n","").strip()
+
+	log_file = open("log_data.json")
+	log_json = log_file.read()
+	log_json = log_json.replace("'",'"')
+	log_json = json.loads(log_json)
+
+	target_bucket = "hyper-threading-automation-logs"
+	target_date = str(datetime.today().strftime('%Y-%m-%d(%H:%M:%S)'))
+	final_filename = f'hyperthreading-ephemeral({target_date}).json'
+
+	s3_resource = boto3.resource('s3', aws_access_key_id=goss_key, aws_secret_access_key=goss_secret)
+	folder_filename = f'ephemeral-disks-log/{final_filename}'
+
+	s3object = s3_resource.Object(target_bucket, folder_filename)
+
+	s3object.put(Body=(bytes(json.dumps(log_json, indent=4).encode('UTF-8'))))
+
+
+data_log = main()
+write_json_s3(data_log)
