72be2b7b2d196729c15ceb8e48fc1fd5bcee81db
Vince Agbayani <JohnVincentAgbayani@Deltek.com>
2024-05-03 16:57:21 +0800

    fixed log file reference in uploader

--- s3_log_helper.py
+++ s3_log_helper.py
@@ -3,7 +3,7 @@ import json
 from datetime import datetime	
 
 def write_json_s3():
-	log_file = open("sample_data.json")
+	log_file = open("log_data.json")
 	log_json = log_file.read()
 	log_json = log_json.replace("'",'"')
 	print(log_json)
